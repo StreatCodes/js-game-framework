efe075d6198fbd6a28bb10fdd7e221ec9fd0eee7
StreatCodes <matt.r.streatfield@gmail.com>
2017-02-13 20:53:07 +1100

    Sprites now animate properly

--- src/framework.js
+++ src/framework.js
@@ -6,12 +6,11 @@ let _canvas;
 let _context;
 let _spriteMap = new Map();
 let _loop;
-let _timer;
 
-export function loadImage(url){
-    if(!_spriteMap.has(url)){
+export function loadImage(url) {
+    if (!_spriteMap.has(url)) {
         //todo improve
-        let img = new Image(); 
+        let img = new Image();
         img.src = url;
 
         _spriteMap.set(url, img);
@@ -29,7 +28,7 @@ export function init(canvas, init, mainLoop) {
     run();
 }
 
-function run(){
+function run() {
     Time.update();
     _context.clearRect(0, 0, _canvas.width, _canvas.height);
     _loop();
@@ -40,32 +39,57 @@ function run(){
 
 
 export class GameObject {
-    constructor(sprite){
+    constructor(sprite) {
         this.sprite = sprite;
     }
 
-    update(){
+    update() {
 
     }
 
-    draw(){
+    draw() {
         this.sprite.draw();
     }
 }
 
 export class Sprite {
-    constructor(context, url, pos, cords){
+    constructor(context, url, pos, cords = [], fps = 24) {
         this.context = context;
         this.image = loadImage(url);
         this.pos = pos;
         this.cords = cords;
-
-        //TODO coords
+        this.allowedFrameTime = 1.0 / fps;
+        this.frame = 0;
+        this.frameTime = 0.0;
     }
 
-    draw(){
-        this.context.drawImage(this.image, this.pos.x, this.pos.y);
-        // dx, dy, dWidth, dHeight);
+    //add animation frame
+    addFrame(sourceVec, dimensionVec) {
+        this.cords.push({
+            source: sourceVec,
+            dimensions: dimensionVec
+        });
+    }
+
+    draw() {
+        //draws image to context
+        this.context.drawImage(this.image,
+            this.cords[this.frame].source.x,        //source X
+            this.cords[this.frame].source.y,        //source Y
+            this.cords[this.frame].dimensions.x,    //source width
+            this.cords[this.frame].dimensions.y,    //source height
+            this.pos.x,                             //destination X
+            this.pos.y,                             //destination Y
+            this.cords[this.frame].dimensions.x,    //destination width
+            this.cords[this.frame].dimensions.y);   //destination height
+
+        this.frameTime += Time.dt();
+
+        //handle animation
+        if (this.frameTime > this.allowedFrameTime) {
+            this.frameTime = 0.0;
+            this.frame++;
+            this.frame > this.cords.length - 1 ? this.frame = 0 : null;
+        }
     }
 }
-
--- src/test-game.js
+++ src/test-game.js
@@ -2,7 +2,17 @@ import * as framework from './framework';
 import Victor from 'Victor';
 
 let img = framework.loadImage("images/spritesheet-demo.png");
-let sprite = new framework.Sprite(document.getElementById("canvas").getContext('2d'), "images/spritesheet-demo.png", new Victor(0,0), new Victor(200, 200));
+let sprite = new framework.Sprite(document.getElementById("canvas").getContext('2d'), "images/spritesheet-demo.png", new Victor(0,0), [], 12);
+sprite.addFrame(new Victor(0, 89), new Victor(34, 44));
+sprite.addFrame(new Victor(32, 89), new Victor(36, 44));
+sprite.addFrame(new Victor(71, 89), new Victor(49, 44));
+sprite.addFrame(new Victor(124, 89), new Victor(38, 44));
+sprite.addFrame(new Victor(161, 89), new Victor(29, 44));
+sprite.addFrame(new Victor(194, 89), new Victor(27, 44));
+sprite.addFrame(new Victor(225, 89), new Victor(29, 44));
+sprite.addFrame(new Victor(258, 89), new Victor(39, 44));
+sprite.addFrame(new Victor(301, 89), new Victor(33, 44));
+sprite.addFrame(new Victor(336, 89), new Victor(27, 44));
 
 function init() {
 }
